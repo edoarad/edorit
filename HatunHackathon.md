ad742e31072256b271ca26bb3c26653e96558f93
edoarad <edoarad92@gmail.com>
2020-09-20 15:12:58 +0300

    added HatunHackathon.md

---
+++ HatunHackathon.md
@@ -0,0 +1,30 @@
+# Set up
+
+
+## Common problems
+* On windows, pip install may fail on `rcssmin` and others if there is no Visual C++ Build Tools.
+    * Need to install Version 14. It is big so it may take some time.
+
+# HH logistics
+* Suggest that people will team up in pairs or more.
+* Have a table of tasks, each task may have several people listed. 
+
+# Design Decisions
+* Should be available both in PCs and phones.
+
+# Conventions and decisions
+* Guests enter for  a single session. They can enter again at a later date, but no user maintenance is needed.
+
+# Tasks before HatunHackathon
+## py/js/technical
+* Make sure that the infrastructure we use fits the usages we want.
+* Prepare file/framework for common logic.
+* prepare outside sources that are common throughout several views.
+* Decide on language. Can we use both RTL and LTR? 
+* Create an example page. Perhaps reception.
+* Get a domain
+
+## Design
+* Prepare framework for common design
+* Set up early CSS templates.
+* Find a bank of images.
--- website/urls.py
+++ website/urls.py
@@ -16,11 +16,13 @@ urlpatterns = [
     #     yes button - orit with cookies. No button, Edo with lattice.
     # Bar - Lots of Corona bottles. Can drink shots - each shot, the screen starts to be more fuzzy and move around for the entire wedding.
     # Talk with people - Rabbi, grandmas, parents, ...
-    # Coronavirus attack!! - suddenly, the world turns green. An alert message tells you that there is a virus attack, and you need to fire UV on little covid viruses to kill them all.
+    # Coronavirus attack!! - suddenly, the world turns green. An alert message tells you that there is a virus attack, 
+    #       and you need to fire UV on little covid viruses to kill them all.
     # Side adventures - in the periphery, mom drinks and becomes drunk more and more over time.
     # Patting zone - place where kittens purr
     # Desserts - gallery of Orit's dessert photos.
-    # Dance floor - people can take videos of themselves and then the dance floor will be filled with GIFs of people dancing. (Perhaps some moderation would be helpful)
+    # Dance floor - people can take videos of themselves and then the dance floor will be filled with GIFs of people dancing. 
+    #       (Perhaps some moderation would be helpful)
     # Magnet board - contains a bunch of photos of people with blank faces. people can add their faces in via webcam and save the photo.
 
 
