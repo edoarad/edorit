9a5d459b6df7408e15d4f13f6cdefbac9f28f72e
Shay Ben Moshe <shayfalador@gmail.com>
2020-09-20 16:25:00 +0300

    form looks nicer

--- website/forms.py
+++ website/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class ReceptionForm(forms.Form):
-    name = forms.CharField(label='name', max_length=100, initial="Full Name")
-    table_name = forms.CharField(label='table_name', max_length=100, initial="Table Name")
+    name = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={"placeholder": "Full Name", "class": "form-control mb-2 mr-sm-2"}))
+    table_name = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={"placeholder": "Table Name", "class": "form-control mb-2 mr-sm-2"}))
