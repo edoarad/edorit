b2966b5ad60adcb82bada4bab4b9122af2d78f8e
Geva Kipper <gevakip@gmail.com>
2020-10-09 11:57:36 +0300

    Add id to guest session

--- website/models.py
+++ website/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 
-
-# class Item(models.Model):
-#     text = models.CharField(max_length=1023)
-
-
 class Guest(models.Model):
     name = models.CharField(max_length=1023)
     table_name = models.CharField(max_length=1023)
