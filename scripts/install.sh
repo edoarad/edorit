b867479403b37a445624c96dfbbf3440c91afb5c
shaybenmoshe <shaybm9@gmail.com>
2020-10-09 10:55:31 +0300

    initiate db on install.bat/sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,3 +8,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 python -m virtualenv .env --prompt='[edorit] '
 .env/bin/pip install -U pip
 .env/bin/pip install -r requirements.txt
+
+python3 manage.py makemigrations website
+python3 manage.py migrate
